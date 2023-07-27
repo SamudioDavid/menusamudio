caf99e8b932ea86771df7e659200f788493e2afc
SamudioDavid <140515260+SamudioDavid@users.noreply.github.com>
2023-07-27 08:27:46 -0300

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,24 @@
+Imports System
+Module Program
+    Sub main(Arg As)
+        Dim variable
+        Console.WriteLine("")
+        Do Until variable
+            Console.WriteLine()
+            Console.WriteLine()
+            Console.WriteLine()
+            Console.WriteLine()
+            Console.WriteLine()
+            variable
+            If variable Then
+                Console.WriteLine()
+            ElseIf variable Then
+                Console.WriteLine()
+            ElseIf variable Then
+                Console.WriteLine()
+            End If
+            Console.WriteLine()
+        Loop
+
+    End Sub
+    End Sub
